b37c7ff698b7f0686a4175d74a05a53c9e582f46
Davi Gomes <davigomes.m1@gmail.com>
2023-04-14 14:36:41 -0300

    update strings

--- JS/strings.js
+++ JS/strings.js
@@ -1,2 +1,8 @@
 console.log('"olá mundo"')
-//Uso de Strings e aprendizagem
+//Uso de Strings e aprendizagem (' " `)
+console.log(`
+Olam undo de novo
+`)
+
+//Uso de {} dentro das strings (${})
+console.log(`olá mundo ${ 3 }`)
