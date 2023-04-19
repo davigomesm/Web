dab243e76d898f90cd118e7d0247927fc4ff42a8
Davi Gomes <davigomes.m1@gmail.com>
2023-04-19 12:47:58 -0300

    Update / aprendendo web

--- JS/funcoes2.js
+++ JS/funcoes2.js
@@ -3,3 +3,13 @@ var soma = function(num1, num2){
 }
 soma(2, 2)
 console.log(resultado)
+
+var soma = function(num1, num2){
+    let resultado = num1 + num2
+    return resultado
+}
+var num1 = 1
+var num2 = 2
+console.log(`O valor do primeiro numero é ${num1}`)
+console.log(`O valor do segundo numero é ${num2}`)
+console.log(`E a soma dos dois valores é ${soma(num1, num2)}`)
---
+++ JS/rowfuncoes.js
@@ -0,0 +1,6 @@
+let num1 = 1
+let num2 = 2
+soma = function() {
+    console.log(num1 + num2)
+}
+soma(num1, num2)
