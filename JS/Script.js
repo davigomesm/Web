3c30e3ed711ef2f3751cade4895fec54ed726af0
Davi Gomes <davigomes.m1@gmail.com>
2023-04-19 10:55:56 -0300

    Update / aprendendo js

--- JS/Script.js
+++ JS/Script.js
@@ -1,11 +1,9 @@
-const animais = [
-    "gato",
-    {
-        animal: "cachorro",
-        nome: "Mel",
-        idade: 4,
-    },
-    "tartaruga"
-]
-console.log(animais[1])
-console.log(animais[1].idade) 
+var soma = function(num1, num2){
+    resultado = num1 + num2
+    return resultado
+}
+var num1 = 1
+var num2 = 2
+console.log(`O valor do primeiro numero é ${num1}`)
+console.log(`O valor do segundo numero é ${num2}`)
+console.log(`E a soma dos dois valores é ${soma(num1, num2)}`)
---
+++ JS/funcoes2.js
@@ -0,0 +1,5 @@
+var soma = function(num1, num2){
+    resultado = num1 + num2
+}
+soma(2, 2)
+console.log(resultado)
