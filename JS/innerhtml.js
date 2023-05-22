52036122ba8c9b7141b42c8a8698adef1a7e95d8
Davi Gomes <davigomes.m1@gmail.com>
2023-05-22 12:28:20 -0300

    Study Js

---
+++ JS/innerhtml.js
@@ -0,0 +1,8 @@
+<script>
+    var output = document.querySelector("#output");
+    var mensagem = "";
+    for (i = 1; i < 10; i++){
+    mensagem += "i: " + i + "<br>";
+    }   
+    output.innerHTML = mensagem;
+</script>
